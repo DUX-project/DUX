f3fee099ee1c312d11a591e1e424019190ae7d3a
WALDEMAR777 <our-cloud@inbox.ru>
2015-12-14 18:12:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,514 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DUX MD</title>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" type="text/css" href="css/media.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<div id="black"></div>
+		<div id="window_sign">
+			<div class="sign-up-form">
+				<div class="close-window"></div>
+				<p class="modal-h1">Get registered</p>	
+				<div class="facebook-button">Continue with Facebook</div>
+				<span>or</span>
+				<form class="modal-form">
+					<input class="modal-input" name="sign-up-email" type="e-mail" placeholder="E-mail" required>
+					<input class="modal-input" name="sign-up-pass" type="password" placeholder="Create password" required>
+					<input class="modal-input modal-submit-sign-up" name="sign-up-submit" type="submit" value="Sign up"> 
+				</form>
+				<p class="text"><a href="#">I am registered user ></a></p>
+			</div>
+		</div>
+		<div id="window_log">
+			<div class="sign-up-form">
+				<div class="close-window"></div>
+				<p class="modal-h1">Authorize</p>	
+				<div class="facebook-button">Continue with Facebook</div>
+				<span>or</span>
+				<form class="modal-form">
+					<input class="modal-input" name="log-in-login" type="text" placeholder="Login" required>
+					<input class="modal-input" name="log-in-pass" type="password" placeholder="Create password" required>
+					<input class="modal-input modal-submit-sign-up" name="log-in-submit" type="submit" value="Log in"> 
+				</form>
+				<p class="text"><a href="#">Sign up ></a></p>
+			</div>
+		</div>
+		<header>
+			<div class="header_container">
+				<a href="#">
+					<div class="logo-link"></div>
+				</a>
+				<div class="header_buttons">
+					<a href="#">
+						<div class="header_button sign_up">
+							Sign up
+						</div>
+					</a>
+					<a href="#">
+						<div class="header_button log_in">
+							Log in
+						</div>
+					</a>
+				</div>
+			</div>
+			<nav class="top-nav-menu">
+				<ul class="top-nav-list">
+					<a href="#"><li class="top-nav">
+						events
+					</li></a>
+					<a href="#"><li class="top-nav">
+						write a review
+					</li></a>
+					<a href="#"><li class="top-nav">
+						places
+					</li></a>
+					<a href="#"><li class="top-nav">
+						about us
+					</li></a>
+					<a href="#"><li class="top-nav">
+						contacts
+					</li></a>
+				</ul>
+			</nav>
+			<div class="search-panel">
+				<p>
+					3,500 Places to visit
+				</p>
+				<form>
+					<input type="text" placeholder="Search" name="search" required>
+					<input type="submit" name="submit-search-form" value="">
+				</form>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<nav class="services">
+						<ul class="sevices-nav">
+							<li class="service-list ico-1">
+								<p class="name">
+									Restaraunts
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Pizza</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Kebab</p>
+										<p class="subreview">1,036 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Burger</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-2">
+								<p class="name">
+									Food
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">European</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Russian</p>
+										<p class="subreview">1,036 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Japanese</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">French</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Italian</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-3">
+								<p class="name">
+									Nightlife
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Non stop</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">18+</p>
+										<p class="subreview">1,036 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Face control</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-4">
+								<p class="name">
+									Shopping
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Dress</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Shoes</p>
+										<p class="subreview">1,036 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Clothes</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-5">
+								<p class="name">
+									Bars
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Beer</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-6">
+								<p class="name">
+									Coffe & Tea
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Coffe</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Tea</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-7">
+								<p class="name">
+									Beauty & Spas
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Solarium</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+								</ul>
+							</li>
+							<li class="service-list ico-8">
+								<p class="name">
+									Health and Medical
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Hospital</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>	
+								</ul>
+							</li>
+							<li class="service-list ico-9">
+								<p class="name">
+									Home Services
+								</p>
+								<p class="review">
+									10,092 reviewed
+								</p>
+								<ul class="subnav">
+									<li class="subli">
+										<p class="subname">Plumber</p>
+										<p class="subreview">8,156 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Electrician</p>
+										<p class="subreview">1,036 reviewed</p>
+									</li>
+									<li class="subli">
+										<p class="subname">Husband for an hour</p>
+										<p class="subreview">834 reviewed</p>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col-lg-6 middle-block">
+					<p class="h1-middle-block">
+						Best Restaurants
+					</p>
+						<div class="row restaurant">
+							<div class="col-lg-3 restaurant-photo" style="background-image: url('img/restaurant-1.jpg');"></div>
+							<div class="col-lg-9 restaurant-description">
+								<p class="title">
+									<a href="restaurant.php">Steak House</a>
+								</p>
+								<p class="review">
+									106 reviews
+								</p>
+								<p class="restaurant-stars star-5"></p>
+								<div class="row owner">
+									<div class="col-lg-2">
+										<div class="user-photo" style="background-image:url('img/a1.jpg');"></div>
+									</div>
+									<div class="col-lg-10">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row restaurant">
+							<div class="col-lg-3 restaurant-photo" style="background-image: url('img/restaurant-2.jpg');"></div>
+							<div class="col-lg-9 restaurant-description">
+								<p class="title">
+									<a href="restaurant.php">BBQ Brothers</a>
+								</p>
+								<p class="review">
+									244 reviews
+								</p>
+								<p class="restaurant-stars star-4"></p>
+								<div class="row owner">
+									<div class="col-lg-2">
+										<div class="user-photo" style="background-image:url('img/a2.jpg');"></div>
+									</div>
+									<div class="col-lg-10">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row restaurant">
+							<div class="col-lg-3 restaurant-photo" style="background-image: url('img/restaurant-3.jpg');"></div>
+							<div class="col-lg-9 restaurant-description">
+								<p class="title">
+									<a href="restaurant.php">Holm's Pub</a>
+								</p>
+								<p class="review">
+									106 reviews
+								</p>
+								<p class="restaurant-stars star-3"></p>
+								<div class="row owner">
+									<div class="col-lg-2">
+										<div class="user-photo" style="background-image:url('img/a3.jpg');"></div>
+									</div>
+									<div class="col-lg-10">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row restaurant">
+							<div class="col-lg-3 restaurant-photo" style="background-image: url('img/restaurant-4.jpg');"></div>
+							<div class="col-lg-9 restaurant-description">
+								<p class="title">
+									<a href="restaurant.php">Tom's Restaurant</a>
+								</p>
+								<p class="review">
+									106 reviews
+								</p>
+								<p class="restaurant-stars star-2"></p>
+								<div class="row owner">
+									<div class="col-lg-2">
+										<div class="user-photo" style="background-image:url('img/a4.jpg');"></div>
+									</div>
+									<div class="col-lg-10">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row restaurant">
+							<div class="col-lg-3 restaurant-photo" style="background-image: url('img/restaurant-5.jpg');"></div>
+							<div class="col-lg-9 restaurant-description">
+								<p class="title">
+									<a href="restaurant.php">Origato & Co</a>
+								</p>
+								<p class="review">
+									106 reviews
+								</p>
+								<p class="restaurant-stars star-1"></p>
+								<div class="row owner">
+									<div class="col-lg-2">
+										<div class="user-photo" style="background-image:url('img/a5.jpg');"></div>
+									</div>
+									<div class="col-lg-10">
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="see-more">
+							<span>
+								See more
+							</span>
+						</div>
+						<div class="hr"></div>
+						<p class="h1-middle-block">
+							Recent Activity
+						</p>
+						<div class="recent-user">
+							<div class="row">
+								<div class="col-lg-1">
+									<img src="img/a1.jpg">
+								</div>
+								<div class="col-lg-11">
+									<p class="user">
+										<a href="#">Mary Jane</a> reviewed <a href="#">Steak House</a>
+									</p>
+									<p class="restaurant-stars recent-stars star-5"></p>
+								</div>
+								<p class="user-discription">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <a href="">more...</a>
+								</p>
+							</div>
+						</div>
+						<div class="recent-user">
+							<div class="row">
+								<div class="col-lg-1">
+									<img src="img/a1.jpg">
+								</div>
+								<div class="col-lg-11">
+									<p class="user">
+										<a href="#">Mary Jane</a> reviewed <a href="#">Steak House</a>
+									</p>
+									<p class="restaurant-stars recent-stars star-3"></p>
+								</div>
+								<p class="user-discription">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <a href="">more...</a>
+								</p>
+							</div>
+						</div>
+						<div class="recent-user">
+							<div class="row">
+								<div class="col-lg-1">
+									<img src="img/a1.jpg">
+								</div>
+								<div class="col-lg-11">
+									<p class="user">
+										<a href="#">Mary Jane</a> reviewed <a href="#">Steak House</a>
+									</p>
+									<p class="restaurant-stars recent-stars star-1"></p>
+								</div>
+								<p class="user-discription">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <a href="">more...</a>
+								</p>
+							</div>
+						</div>
+						<div class="see-more">
+							<span>
+								See more
+							</span>
+						</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="weather">
+						<p class="today">Today, 14 August</p>
+						<p class="degree">+37° C</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-2">
+						<a href="#">
+							<div class="footer-logo"></div>
+						</a>
+					</div>
+					<div class="col-lg-2">
+						<ul class="footer-list">
+							<li>Support</li>
+							<li>Terms of Service</li>
+							<li>Security</li>
+							<li>Privacy Policy</li>
+						</ul>
+					</div>
+					<div class="col-lg-2">
+						<ul class="footer-list">
+							<li>About</li>
+							<li>Blog</li>
+							<li>Contacts</li>
+						</ul>
+					</div>
+					<div class="col-lg-2">
+						<ul class="footer-list">
+							<li>Get in touch</li>
+							<li>Contact us</li>
+						</ul>
+						<div class="hub fb"></div>
+						<div class="hub vk"></div>
+						<div class="hub gp"></div>
+					</div>
+					<div class="col-lg-4">
+						<ul class="footer-list">
+							<li>Subscribe to newsletter</li>
+						</ul>
+						<form>
+							<input type="e-mail" name="subsriber-email" placeholder="Your e-mail" required>
+							<input type="submit" value="SUBSCRIBE">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="copy">
+				Copyright © 2015 Dux
+			</div>
+		</footer>
+		<script src="js/jquery.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
